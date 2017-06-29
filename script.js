c14ea8a625353a2a6a8d2b7f58cddc78280ea860
Jackie He <Jackie.He8@gmail.com>
2017-06-29 19:57:36 +0000

    we worked hard

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
+    $(".social_img").hover(function() {
+        $(this).animate({
+            paddingTop: "15px"
+        }, 100);
+    }, function() {
+        $(this).animate({
+            paddingTop: "10px"
+        }, 100)
+    });
 
-   
-    
+    $('#myModal').on('show.bs.modal', function(event) {
+        var modal = $(this)
+        modal.find('#content').html("Hello " + $('#name').val() + ", thank you for signing up! We will send an email to " + $('#email').val() + " shortly.");
 
-});
+    })
+})
